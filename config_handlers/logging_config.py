41c3e5430a166fb7d162960a60ae062714b5d5b8
dyuzha <matvey10d@gmail.com>
2025-04-29 18:55:24 +0300

    Улучшил считывание конфига
    Теперь, если файл не найден, он автоматически создасться

--- config_handlers/logging_config.py
+++ config_handlers/logging_config.py
@@ -9,21 +9,29 @@ LOG_CONF = environ.get('GLPI_TG_LOG_CONF') or \
 
 def setup_logging():
     """Инициализация системы логирования"""
-    # Создаем папку для логов, если ее нет
-    # LOG_DIR = os.environ.get('GLPI_TG_LOG_DIR') or Path("logs")
-    # LOG_DIR.mkdir(exist_ok=True)
-
     try:
         with open(Path(LOG_CONF), 'r', encoding='utf-8') as f:
             logging_config = json.load(f)
     except FileNotFoundError:
         raise Exception(f"Файл конфигурации {LOG_CONF} не найден")
     except json.JSONDecodeError as e:
-        raise Exception(f"Ошибка в JSON: {e.msg} (строка {e.lineno}, столбец {e.colno})")
+        raise Exception(f"Ошибка в JSON: {e.msg} (строка {e.lineno}, \
+                столбец {e.colno})")
+
+    # Если есть файловый обработчик, создаем папки и файл заранее
+    if "handlers" in logging_config and "file" in logging_config["handlers"]:
+        log_conf_path = Path(logging_config['handlers']['file']['filename'])
+        log_conf_path.parent.mkdir(parents=True, exist_ok=True)  # Создаем папки
+        if not log_conf_path.exists():
+            log_conf_path.touch()
 
     # Применяем конфигурацию
     logging.config.dictConfig(logging_config)
 
     logger = logging.getLogger(__name__)
     logger.info("Logging configured successfully")
+
+    # Если определен файл, для создания логов: логируем это
+    if log_conf_path:
+        logger.debug(f"Log file initialized: {log_conf_path}")
     return logger
