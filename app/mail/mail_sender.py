d4c39c788d0245e698393fd6c556e4ad3e9c9b59
dyuzha <matvey10d@gmail.com>
2025-05-05 16:28:31 +0300

    Сделал класс mail_sender поддерживающим ассинхронную отправку

--- app/mail/mail_sender.py
+++ app/mail/mail_sender.py
@@ -1,8 +1,9 @@
-import smtplib
+import aiosmtplib
 from email.mime.text import MIMEText
 import random
 import string
 from config_handlers import MAIL_DATA
+from typing import Optional
 import logging
 
 logger = logging.getLogger(__name__)
@@ -14,13 +15,15 @@ class MailSender():
         self.smtp_port = mail_config['smtp_port']
         self.smtp_username = mail_config['smtp_username']
         self.smtp_password = mail_config['smtp_password']
+        self.use_tls = mail_config['use_tls']
 
     def generate_confirmation_code(self, length=8) -> str:
         """Генерация случайного кода подтверждения"""
         characters = string.digits  # Используем только цифры
         return ''.join(random.choice(characters) for _ in range(length))
 
-    def build_confirm_mail(self, code, email_to) -> None:
+    def _build_confirm_mail(self, code: str, email_to: str) -> MIMEText:
+        """Создание email сообщения"""
         subject = "Код подтверждения авторизации в Telegram-боте **ПРОФИТ**"
         body = f"""Здравствуйте,\n
         Ваш код подтверждения для телеграмм-бота: {code}\n
@@ -31,20 +34,28 @@ class MailSender():
         msg['Subject'] = subject
         msg['From'] = self.smtp_username
         msg['To'] = email_to
+        return msg
 
-    def send_confirmation_email(self, msg: MIMEText) -> bool:
-        """Отправка письма с кодом подтверждения"""
+    async def send_confirmation_email(self, email_to: str) -> Optional[str]:
+        """Асинхронная отправка письма с кодом подтверждения"""
+        code = self.generate_confirmation_code()
+        msg = self._build_confirm_mail(code, email_to)
+
         try:
             # Подключение и отправка
-            with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
-                server.starttls()
-                server.login(self.smtp_username, self.smtp_password)
-                server.send_message(msg)
+            async with aiosmtplib.SMTP(
+                hostname=self.smtp_server,
+                port=self.smtp_port,
+                use_tls=self.use_tls
+            ) as server:
+                await server.login(self.smtp_username, self.smtp_password)
+                await server.send_message(msg)
+
             logger.info(f"Письмо с кодом подтверждения успешно отправлено \
                     на {msg['To']}")
-            return True
+            return code
         except Exception as e:
             logger.warning(f"Ошибка при отправке письма на {msg['To']}: {e}")
-            return False
+            return None
 
 mail_sender = MailSender(MAIL_DATA)
