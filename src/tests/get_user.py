c72e22a587345ac859efec07a4f454b2a547dbe6
dyuzha <dyuzha@MacBook-Pro-dyuzha.local>
2025-05-23 20:59:31 +0300

    ..

--- src/tests/get_user.py
+++ src/tests/get_user.py
@@ -1,29 +1,17 @@
-from glpi_bot.glpi.models import TicketBuilder
+from glpi_bot.glpi.session import GLPISessionManager, GLPIBase
+from glpi_bot.services import GLPIService
 from glpi_bot.config_handlers import GLPI_DATA
 
 
-ticket_data = {
-        "login": "dyuzhev_mn",
-        "name": "test_title",
-        "content": 'test_description',
-        "type": 1, # 1 для инцидента, 2 для запроса
-        # "urgency": 3, # Срочность (1-5)
-        # "impact": 3, # Влияние (1-5)
-        # "priority": 3, # Приоритет (1-5)
-        # "requesttypes_id": 14, # Источник запроса
-        # "itilcategories_id": 1, ID Категории,
-        # "_users_id_requester": 291, # ID пользователя-заявителя
-        # "entities_id": 10  # ID организации (0 для корневой)
-    }
+# Инициализация glpi
+glpi = GLPIBase(**GLPI_DATA)
+glpi_session_manager = GLPISessionManager(glpi)
+glpi_service = GLPIService(glpi_session_manager)
+
 
 login = "dyuzhev_mn"
 
+
 # Получаем пользователя GLPI
-with TicketBuilder(**GLPI_DATA) as glpi:
-    result = glpi.get_user("dyuzhev_mn")
-    print(result.get_id())
-
-# with TicketBuilder(**GLPI_DATA) as glpi:
-#     result = glpi.get_user_()
-
-
+result = glpi_service.get_user("akimova_vi")
+print(result)
