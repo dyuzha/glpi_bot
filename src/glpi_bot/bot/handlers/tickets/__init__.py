7b72fe06f0091796d32e82330a57215acd8594a9
dyuzha <dyuzha@MacBook-Pro-dyuzha.local>
2025-06-08 23:00:41 +0300

    ..

--- src/glpi_bot/bot/handlers/tickets/__init__.py
+++ src/glpi_bot/bot/handlers/tickets/__init__.py
@@ -3,16 +3,18 @@ from .models import DynamicBotMessage
 from glpi_bot.bot.handlers.tickets.models import BaseForkMaker
 from glpi_bot.bot.keyboards import base_buttons
 
-from .base import router as base_router
-from .final import router as final_router
-from .fork_makers import router as fork_makers_router
-from .start_ticket import router as start_ticket_router
-
 bot_message = DynamicBotMessage()
 incident_1c_fork_maker = BaseForkMaker(base_buttons=base_buttons)
 inc_it_fork_maker = BaseForkMaker(base_buttons=base_buttons)
 request_it_fork_maker = BaseForkMaker(base_buttons=base_buttons)
 request_1c_fork_maker = BaseForkMaker(base_buttons=base_buttons)
+
+
+from .base import router as base_router
+from .final import router as final_router
+from .fork_makers import router as fork_makers_router
+from .start_ticket import router as start_ticket_router
+
 
 from .forks import inc_1c
 from .forks import inc_it
