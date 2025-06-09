b6f85129c9843ee0c9628a927688e1e569d962b4
dyuzha <dyuzha@MacBook-Pro-dyuzha.local>
2025-06-09 21:53:12 +0300

    ..

--- src/glpi_bot/bot/handlers/tickets/send_ticket.py
+++ src/glpi_bot/bot/handlers/tickets/send_ticket.py
@@ -24,6 +24,7 @@ def setup_send_ticket(glpi: GLPITicketManager):
 
         try:
             d = await state.get_data()
+            logger.debug(f"State data: {d}")
         except Exception as e:
             logger.exception(f"Ошибка при получении состояния: {e}")
             await callback.answer("Произошла ошибка. Попробуйте сначала.")
