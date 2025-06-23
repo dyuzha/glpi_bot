f6504aee46f53f329bd214c5b1a66f3d64d5582f
dyuzha <matvey10d@gmail.com>
2025-06-23 09:56:39 +0300

    Изменил itil_cat запросов на актуальные

--- src/glpi_bot/bot/handlers/tickets/forks/req_1c.py
+++ src/glpi_bot/bot/handlers/tickets/forks/req_1c.py
@@ -3,7 +3,7 @@
 import logging
 from typing import Callable, Optional
 from aiogram.fsm.context import FSMContext
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, Message
 
 from glpi_bot.bot.handlers.models import BaseFlowCollector
 from glpi_bot.bot.handlers.tickets.steps import description_step
@@ -25,7 +25,9 @@ async def call_description(
     await state.update_data(title = category)
     await bot_message.add_field(state, "Категория", category)
 
-    await description_step.show(callback.message, state, prompt)
+    if isinstance(callback.message, Message):
+        await description_step.show(callback.message, state, prompt)
+
     await callback.answer()
 
 
@@ -39,19 +41,20 @@ def local_register(call_code: str, key_prompt: str,  itilcategories_id: int, fun
     )
 
 puncts = [
-        ["develop", "Доработка/разработка (отчеты, формы...)", 42],
-        ["2", "Сервисы (ЭДО, 1C отчетность...)", 42],
-        ["3", "Оборудование (Кассы, весы, ТСД...)", 42],
+        ["develop", "Доработка/разработка (отчеты, формы...)", 40],
+        ["2", "Сервисы (ЭДО, 1C отчетность...)", 76],
+        ["3", "Оборудование (Кассы, весы, ТСД...)", 77],
         ["4", "Визуализация данных, списков, таблиц", 42],
-        ["5", "Типовые решения (ЗУП, бухгалтерия...)", 42],
-        ["6", "Экспорт/импорт данных", 42],
-        ["7", "Обновление конфигурации", 42],
-        ["8", "Покупка продуктов (лицензии, конфигурации...)", 42],
-        ["9", "Добавление, удаление, восстановление баз и пользователей", 42],
-        ["10", "Производительность", 42],
+        ["5", "Типовые решения (ЗУП, бухгалтерия...)", 79],
+        ["6", "Экспорт/импорт данных", 56],
+        ["7", "Обновление конфигурации", 31],
+        ["8", "Покупка продуктов (лицензии, конфигурации...)", 81],
+        ["9", "Добавление, удаление, восстановление баз и пользователей", 19],
+        ["10", "Производительность", 80],
          ]
 
 args = [local_register(*punct) for punct in puncts]
+
 
 def build_flow(base_buttons: list) -> BaseFlowCollector:
     flow_collector = BaseFlowCollector(base_buttons=base_buttons)
