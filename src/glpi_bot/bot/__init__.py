cae3d9b4a3113efb0f82e61adaafd0365f8c44aa
dyuzha <matvey10d@gmail.com>
2025-06-24 16:02:16 +0300

    ref

--- src/glpi_bot/bot/__init__.py
+++ src/glpi_bot/bot/__init__.py
@@ -1,16 +1,13 @@
 # bot/__init__.py
 
-import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from glpi_bot.bot.menu import set_bot_commands
+from glpi_bot.bot.menu import set_bot_commands, remove_bot_commands
 from glpi_bot.bot.handlers import register_handlers
 
-
-logger = logging.getLogger(__name__)
 
 def create_bot(services: dict, telegram_token):
     glpi_service = services["glpi_service"]
@@ -30,8 +27,7 @@ def create_bot(services: dict, telegram_token):
 
     async def on_startup():
         """Действия при запуске бота"""
-        # Возможно логика инициализации
-        # logger.info("Меню для бота загруженно")
-        pass
+        await remove_bot_commands(bot)
+        await set_bot_commands(bot)
 
     return bot, dp, on_startup
